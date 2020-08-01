b020f4c92d433d90eadc5cd9bc3d79e4a9dd75bf
HediHargam <60517481+HediHargam@users.noreply.github.com>
2020-08-01 22:43:35 +0200

    suppr plot

--- train.py
+++ train.py
@@ -20,92 +20,18 @@ grid_search = False
 df = pd.read_csv('data.csv')
 
 
-def plot_confusion_matrix(cm,
-                          target_names,
-                          title='Confusion matrix',
-                          cmap=None,
-                          normalize=True):
-    """
-    given a sklearn confusion matrix (cm), make a nice plot
-
-    Arguments
-    ---------
-    cm:           confusion matrix from sklearn.metrics.confusion_matrix
-
-    target_names: given classification classes such as [0, 1, 2]
-                  the class names, for example: ['high', 'medium', 'low']
-
-    title:        the text to display at the top of the matrix
-
-    cmap:         the gradient of the values displayed from matplotlib.pyplot.cm
-                  see http://matplotlib.org/examples/color/colormaps_reference.html
-                  plt.get_cmap('jet') or plt.cm.Blues
-
-    normalize:    If False, plot the raw numbers
-                  If True, plot the proportions
-
-    Usage
-    -----
-    plot_confusion_matrix(cm           = cm,                  # confusion matrix created by
-                                                              # sklearn.metrics.confusion_matrix
-                          normalize    = True,                # show proportions
-                          target_names = y_labels_vals,       # list of names of the classes
-                          title        = best_estimator_name) # title of graph
-
-    Citiation
-    ---------
-    http://scikit-learn.org/stable/auto_examples/model_selection/plot_confusion_matrix.html
-
-    """
-
-    accuracy = np.trace(cm) / float(np.sum(cm))
-    misclass = 1 - accuracy
-
-    if cmap is None:
-        cmap = plt.get_cmap('Blues')
-
-    plt.figure(figsize=(8, 6))
-    plt.imshow(cm, interpolation='nearest', cmap=cmap)
-    plt.title(title)
-    plt.colorbar()
-
-    if target_names is not None:
-        tick_marks = np.arange(len(target_names))
-        plt.xticks(tick_marks, target_names, rotation=90)
-        plt.yticks(tick_marks, target_names)
-
-    if normalize:
-        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
-
-    thresh = cm.max() / 1.5 if normalize else cm.max() / 2
-    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-        if normalize:
-            plt.text(j, i, "{:0.4f}".format(cm[i, j]),
-                     horizontalalignment="center",
-                     color="white" if cm[i, j] > thresh else "black")
-        else:
-            plt.text(j, i, "{:,}".format(cm[i, j]),
-                     horizontalalignment="center",
-                     color="white" if cm[i, j] > thresh else "black")
-
-    plt.tight_layout()
-    plt.ylabel('True label')
-    plt.xlabel('Predicted label\naccuracy={:0.4f}; misclass={:0.4f}'.format(accuracy, misclass))
-    plt.show()
-
-
 class KobeModel():
 
     def __init__(self, target: str = 'shot_made_flag'):
 
         """
-        Class to create dataset for training/testing of Rig State machine learning algorithm.
+        Class to create dataset for training/testing of machine learning algorithm for Kobe Bryant shot prediction.
 
-        It will create X and y, the features and target Rig State based on concatened csv files of tagged datasets
+        It will create X and y, the features and target
         Possibility to tune the dataset with normalization, balance, temporality, encoded target
 
 
-        Return X, y features and target Rig State
+        Return X, y features and target shot result
         """
         self.cols_to_dummies = ['shot_type', 'combined_shot_type', 'shot_zone_area',
                                 'shot_zone_basic', 'shot_zone_range', 'action_type', 'opponent']
@@ -121,7 +47,7 @@ class KobeModel():
         Parameters
         ----------
         df: Dataframe
-        past_mnemonics: list of mnemonics to add temporal feature
+        features: list of mnemonics to add temporal feature
         Returns
         -------
         df: pd.Dataframe with temporal features
@@ -283,11 +209,7 @@ if __name__ == '__main__':
     print({'TEST SCORE': Rforest.score(X_test, y_test), 'TRAIN SCORE': Rforest.score(X_train, y_train)})
 
 
-    conf_mx = confusion_matrix(y_test, eval_y)
-    plot_confusion_matrix(conf_mx,target_names = ['0', '1'],
-                              title='Confusion matrix',
-                              cmap=None,
-                              normalize=False)
+    print(confusion_matrix(y_test, eval_y))
 
     print(classification_report(y_test, eval_y))
 
