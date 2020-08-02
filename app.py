93b70de2a16b4a0ce90fa89d76dcfd00d8d8140a
HediHargam <60517481+HediHargam@users.noreply.github.com>
2020-08-02 20:42:04 +0200

    change model

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ from train import KobeModel
 import pandas as pd
 
 app = Flask(__name__)
-model = pickle.load(open('RF_Kobe.pkl', 'rb'))
+Rforest = pickle.load(open('RF_Kobe.pkl', 'rb'))
+lgbm = pickle.load(open('LGBM_Kobe.pkl', 'rb'))
 
 @app.route('/')
 def home():
@@ -19,10 +20,8 @@ def predict():
     data = [["minutes_remaining", "shot_distance", "lat", "lon"], int_features]
     column_names = data.pop(0)
     final_features = pd.DataFrame(data, columns=column_names)
-    final_features, y = KobeModel().transform(final_features)
-    prediction = model.predict(final_features)
-
-    output = round(prediction[0], 2)
+    final_features = KobeModel().transform(final_features)
+    output = ((Rforest.predict_proba(final_features)[:,1] + lgbm.predict_proba(final_features)[:,1])/2).round()
 
     txt = 'score this shot' if output == 1 else 'miss this shot'
 
--- train.py
+++ train.py
@@ -10,14 +10,19 @@ import matplotlib.pyplot as plt
 #from imblearn.over_sampling import RandomOverSampler, SMOTE
 #from imblearn.under_sampling import RandomUnderSampler
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import confusion_matrix, classification_report
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import roc_auc_score
+from lightgbm import LGBMClassifier
 import pickle
 
 grid_search = False
 
 df = pd.read_csv('data.csv')
+
+df.dropna(inplace=True)
 
 
 class KobeModel():
@@ -28,13 +33,14 @@ class KobeModel():
         Class to create dataset for training/testing of machine learning algorithm for Kobe Bryant shot prediction.
 
         It will create X and y, the features and target
-        Possibility to tune the dataset with normalization, balance, temporality, encoded target
+        Possibility to tune the dataset with normalization, balance, or temporality
 
 
         Return X, y features and target shot result
         """
-        self.cols_to_dummies = ['shot_type', 'combined_shot_type', 'shot_zone_area',
-                                'shot_zone_basic', 'shot_zone_range', 'action_type', 'opponent']
+        self.cols_to_dummies = ['shot_type', 'shot_zone_area',
+                                'shot_zone_basic', 'shot_zone_range']
+        self.col_to_label = ['opponent', 'action_type', 'combined_shot_type']
         self.to_norm = []
         self.past_features = []
         self.target = target
@@ -106,8 +112,7 @@ class KobeModel():
 
     def transform(self, df: pd.DataFrame,
                         NORM=False,
-                        TEMPO=False,
-                        BALANCE=False) -> pd.DataFrame:
+                        TEMPO=False) -> pd.DataFrame:
         """
         Create/Modify the dataset according to a feature engineering
         Parameters
@@ -122,14 +127,13 @@ class KobeModel():
         X: pd.Dataframe with feature engineering
         y: the target
         """
-        tempo = df.copy()
         logger.info('TRANSFORMING DATASET')
 
         if 'matchup' in df.columns:
             df['home'] = df['matchup'].str.contains('vs').astype('int')  # 1 if home play else 0
 
         if 'action_type' in df.columns:
-            action_types = df['action_type'].value_counts().index.values[:15]  # 96% of action type are in the 15 first (of 57) actions
+            action_types = df['action_type'].value_counts().index.values[:30]  # 96% of action type are in the 15 first (of 57) actions
             df.loc[~df['action_type'].isin(action_types), 'action_type'] = 'Other'
 
         for col in self.cols_to_dummies:
@@ -137,22 +141,32 @@ class KobeModel():
                 df = pd.concat([df, pd.get_dummies(df[col], prefix=col)], axis=1)
                 df.drop(columns=col, inplace=True,  errors='ignore')
 
+        le = LabelEncoder()
+        for col in self.col_to_label:
+            if col in df.columns:
+                df[col] = le.fit_transform(df[col])
+
         if 'game_date' in df.columns:
             df['game_date'] = pd.to_datetime(df['game_date'])
-            df['exp_year'] = df['game_date'].dt.year - df['game_date'].dt.year.min()
+            df['exp_year'] = df['game_date'].dt.year - df['game_date'].dt.year.min() #number of year of career
             df['game_month'] = df['game_date'].dt.month
-
-        df.drop(columns='game_date', inplace=True, errors='ignore')
+            df.drop(columns='game_date', inplace=True, errors='ignore')
 
         df = self.normalize_features(df, to_norm=self.to_norm) if NORM else df
-
         df = self.add_temporality(df, features=self.past_features) if TEMPO else df
         df = self.shift_values(df, features=self.past_features) if TEMPO else df
 
-        df.drop(columns=['game_event_id', 'game_id', 'team_name', 'team_id', 'matchup', 'shot_id', 'season', 'loc_x', 'loc_y'],
+        df.drop(columns=['game_event_id', 'game_id', 'team_name', 'team_id', 'matchup', 'shot_id', 'season',
+                         'loc_x', 'loc_y', 'playoffs'],
                 inplace=True,  errors='ignore') #Drop usless/redondant features
 
-        df.dropna(inplace=True)
+
+        return df
+
+
+    def fit_transform(self, df, BALANCE=False):
+
+        df = self.transform(df)
 
         X = df.drop(columns=self.target, errors='ignore')
 
@@ -163,23 +177,36 @@ class KobeModel():
         if BALANCE:
             X, y = self.balance_dataset(X, y)
 
-
         return X, y
+
 
 
 if __name__ == '__main__':
 
     ### Model training --> Transform and fit
 
-    X, y = KobeModel().transform(df)
+    X, y = KobeModel().fit_transform(df,
+                                     BALANCE=False)
 
+    X_train, X_test, y_train, y_test = train_test_split(X, y
+                                                        ,test_size=0.3,
+                                                        random_state=seed,
+                                                        shuffle=True)
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=seed, shuffle=True)
+    Rforest = RandomForestClassifier(n_estimators=400,
+                                     random_state=seed,
+                                     min_samples_split=20,
+                                     criterion='gini')
 
-    Rforest = RandomForestClassifier(n_estimators=100, random_state=seed, min_samples_split=20, criterion='gini')
+    lgbm = LGBMClassifier(objective='binary',
+                          max_depth=10,
+                          num_iterations=1000,
+                          learning_rate=0.01)
+
 
     logger.info('Training Model')
     Rforest.fit(X_train, y_train)
+    lgbm.fit(X_train, y_train)
 
 
     if grid_search:
@@ -201,21 +228,23 @@ if __name__ == '__main__':
 
     logger.info('Evaluating Model Results')
 
-    #for name,score in zip(X_test.columns, Rforest.feature_importances_):
-      #print(name,score*100)
+    for name,score in zip(X_test.columns, Rforest.feature_importances_):
+      print(name,score*100)
 
+    eval_y = ((Rforest.predict_proba(X_test)[:,1] + lgbm.predict_proba(X_test)[:,1])/2).round() #Use two models to incease robustness
 
-    eval_y = Rforest.predict(X_test)
     print({'TEST SCORE': Rforest.score(X_test, y_test), 'TRAIN SCORE': Rforest.score(X_train, y_train)})
-
 
     print(confusion_matrix(y_test, eval_y))
 
     print(classification_report(y_test, eval_y))
 
+    print(roc_auc_score(y_test, eval_y))
 
 
     #### Save The final Model ####
 
+   #pickle.dump(Rforest, open('RF_Kobe.pkl', 'wb'))
+   #pickle.dump(Rforest, open('LGBM_Kobe.pkl', 'wb'))
 
-    #pickle.dump(Rforest, open('RF_Kobe.pkl', 'wb'))
+
